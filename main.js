1c367d2f33d3092c8be847b757696bb0cd044890
Andrea Contestabile <andreacontest@gmail.com>
2020-09-02 16:22:47 +0200

    Carousel jQuery (pulsanti + frecce + pallini)

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
-
+  // Evento click TASTO NEXT
   $(".next").click(function() {
     nextImage();
+    nextCircle();
   });
 
+  // Evento click TASTO PREV
   $(".prev").click(function() {
     prevImage();
+    prevCircle();
   });
 
+  // Evento pressione TASTI FRECCIA DESTRA e SINISTRA
   $(document).keydown(function(e) {
 
     var tastoPremuto = e.which;
@@ -18,14 +22,18 @@ $(document).ready(function() {
 
       case frecciaDestra:
         nextImage();
+        nextCircle();
         break;
       case frecciaSinistra:
         prevImage();
+        prevCircle();
         break;
     }
 
   });
 
+  //FUNZIONI
+  // Funzione prossima immagine
   function nextImage() {
 
     var activeImg = $("img.active");
@@ -40,7 +48,22 @@ $(document).ready(function() {
     nextImg.addClass("active");
 
   }
+  // Funzione selezione prossimo pallino
+  function nextCircle() {
 
+    var activeCircle = $(".nav i.fa-circle.active");
+    activeCircle.removeClass("active");
+
+    if (activeCircle.hasClass("last")) {
+      var nextCircle = $(".nav i.fa-circle.first");
+    } else {
+      var nextCircle = activeCircle.next();
+    }
+
+    nextCircle.addClass("active");
+
+  }
+  // Funzione immagine precedente
   function prevImage() {
 
     var activeImg = $("img.active");
@@ -53,6 +76,21 @@ $(document).ready(function() {
     }
 
     prevImg.addClass("active");
+
+  }
+  // Funzione seleziona pallino precedente
+  function prevCircle() {
+
+    var activeCircle = $(".nav i.fa-circle.active");
+    activeCircle.removeClass("active");
+
+    if (activeCircle.hasClass("first")) {
+      var prevCircle = $(".nav i.fa-circle.last");
+    } else {
+      var prevCircle = activeCircle.prev();
+    }
+
+    prevCircle.addClass("active");
 
   }
 
